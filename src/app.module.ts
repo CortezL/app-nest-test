7950cc0a546c1a2255a72026d8d991e03632155e
CortezL <lucascortez.1993@outlook.com>
2021-02-10 00:51:58 -0300

    Adicionando conexão com MongoDB e configurando CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose'
+
 @Module({
-  imports: [TasksModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://ds_user:<password>@cluster0.j8xfz.mongodb.net/<dbname>?retryWrites=true&w=majority'), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
